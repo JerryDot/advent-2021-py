c3eb44b5989c276de0b5e721d3b8dfa1698796f1
Daniel Hunt <dansinh64@gmail.com>
2021-12-06 18:29:59 +0000

    6ing it up

---
+++ fast/day06/day06.py
@@ -0,0 +1,28 @@
+from typing import List
+from collections import Counter
+
+INPUT = [5,3,2,2,1,1,4,1,5,5,1,3,1,5,1,2,1,4,1,2,1,2,1,4,2,4,1,5,1,3,5,4,3,3,1,4,1,3,4,4,1,5,4,3,3,2,5,1,1,3,1,4,3,2,2,3,1,3,1,3,1,5,3,5,1,3,1,4,2,1,4,1,5,5,5,2,4,2,1,4,1,3,5,5,1,4,1,1,4,2,2,1,3,1,1,1,1,3,4,1,4,1,1,1,4,4,4,1,3,1,3,4,1,4,1,2,2,2,5,4,1,3,1,2,1,4,1,4,5,2,4,5,4,1,2,1,4,2,2,2,1,3,5,2,5,1,1,4,5,4,3,2,4,1,5,2,2,5,1,4,1,5,1,3,5,1,2,1,1,1,5,4,4,5,1,1,1,4,1,3,3,5,5,1,5,2,1,1,3,1,1,3,2,3,4,4,1,5,5,3,2,1,1,1,4,3,1,3,3,1,1,2,2,1,2,2,2,1,1,5,1,2,2,5,2,4,1,1,2,4,1,2,3,4,1,2,1,2,4,2,1,1,5,3,1,4,4,4,1,5,2,3,4,4,1,5,1,2,2,4,1,1,2,1,1,1,1,5,1,3,3,1,1,1,1,4,1,2,2,5,1,2,1,3,4,1,3,4,3,3,1,1,5,5,5,2,4,3,1,4]
+
+
+def increment_step(fish: Counter) -> Counter:
+    output = Counter()
+    for key in fish:
+        if key == 0:
+            output[8] = fish[key]
+            output[6] = output.get(6, 0) + fish[key]
+        else:
+            output[key - 1] = output.get(key - 1, 0) + fish[key]
+    return output
+
+def part_one(input: List[int], days):
+    fish = Counter(input)
+    for _ in range(days):
+        fish = increment_step(fish)
+    return sum(fish.values())
+
+
+
+if __name__ == "__main__":
+    print(part_one(INPUT, 80))
+    print(part_one(INPUT, 256))
+    
